b3cf9fbd9e55c5ae51771f0010902a5f4e73e00b
Priyanka Shirur <pshirur323@gmail.com>
2024-12-08 16:49:17 -0800

    new components and styling

--- React_router/src/App.jsx
+++ React_router/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Purple from "./components/Purple";
 import Pink from "./components/Pink";
 import Home from "./components/Home";
+import Yellow from "./components/Yellow";
 
 function App() {
   return (
@@ -12,12 +13,14 @@ function App() {
         <Link to="/">Home</Link>
         <Link to="/purple">Purple</Link>
         <Link to="/pink">Pink</Link>
+        <Link to="/yellow">Yellow</Link>
       </div>
       <div id="main-section">
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/purple" element={<Purple />}></Route>
           <Route path="/pink" element={<Pink />}></Route>
+          <Route path="/yellow" element={<Yellow />}></Route>
         </Routes>
       </div>
     </div>
